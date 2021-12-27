d9216e59ad211298067e7a9ab3c83080823325d6
Manav Doshi <200100094@iitb.ac.in>
2021-12-27 21:59:54 +0530

    Changes in script

--- scriptblue.py
+++ scriptblue.py
@@ -1,4 +1,3 @@
-from random import randint
 corner=["NE","SW","SE","NW"]
 side=["UP","DW","RT","LT"]
 commands=[]
@@ -6,9 +5,12 @@ dic={0:'UP',1:'NW',2:'LT',3:"SW",4:"DW",5:"SE",6:"RT",7:"NE"}
 
 
 def ActRobot(robot):
+        corner=["NE","SW","SE","NW"]
+        side=["UP","DW","RT","LT"]
+        commands=[]
+        dic={0:'UP',1:'NW',2:'LT',3:"SW",4:"DW",5:"SE",6:"RT",7:"NE"}
         if robot.GetVirus() > 1000:
                 robot.DeployVirus(200)
-
         l1=[]
         l1.append(robot.investigate_up())          
         l1.append(robot.investigate_nw())
@@ -19,28 +21,113 @@ def ActRobot(robot):
         l1.append(robot.investigate_right())
         l1.append(robot.investigate_ne())
         for i in range(0,8):
-                if l1[i]=="enemy":
-                        s=robot.GetYourSignal()
-                        robot.setSignal(s[0:4]+print(dic.values(i))+s[6:10]+"EN")
-                        
                 if l1[i]=="enemy-base":
                         s=robot.GetYourSignal()
-                        robot.setSignal(s[0:4]+print(dic.values(i))+s[6:10]+"EB")   
-                        
+                        robot.setSignal(s[0:4]+dic.values(i)+s[6:10]+"EB")
+
+                if l1[i]=="enemy":
+                        s=robot.GetYourSignal()
+                        robot.setSignal(s[0:4]+dic.values(i)+s[6:10]+"EN")
+
+def nextMove(robot):
+        friend_positions = ["friend", "friend-base"]
+        enemy_positions = ["enemy","enemy-base"]
+        empty_positions =  ["blank"]
+        safe_positions = []
+        up = robot.investigate_up()
+        down = robot.investigate_down()
+        left = robot.investigate_left()
+        right = robot.investigate_right()
+        ne = robot.investigate_ne()
+        nw = robot.investigate_nw()
+        se = robot.investigate_se()
+        sw = robot.investigate_sw()
+        resultant_step = {
+                "release_virus" : 0,
+                "enemy_base" : (-1,-1),
+        }
         
+        if up in enemy_positions:
+                if up == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0], t[1]-1)
+                        return resultant_step
+        else:
+                safe_positions.append("up")
+
+        if down in enemy_positions:
+                if down == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0], t[1]+1)
+                        return resultant_step
+        else:
+                safe_positions.append("down")
+        if left in enemy_positions:
+                if left == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0]-1, t[1])
+                        return resultant_step
+        else:
+                safe_positions.append("left")
+        if right in enemy_positions:
+                if right == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0]+1, t[1])
+                        return resultant_step
+        else:
+                safe_positions.append("right")
+        
+        if ne in enemy_positions:
+                if ne == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0]+1, t[1]-1)
+                        return resultant_step
+        # else:
+        #         safe_positions.append("ne")
+        if nw in enemy_positions:
+                if nw == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0]-1, t[1]-1)
+                        return resultant_step
+        # else:
+        #         safe_positions.append("nw")
+        if se in enemy_positions:
+                if se == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0]+1, t[1]+1)
+                        return resultant_step
+        # else:
+        #         safe_positions.append("se")
+        if sw in enemy_positions:
+                if sw == "enemy":
+                        resultant_step["release_virus"] = 1
+                else:
+                        t = robot.GetPosition()
+                        resultant_step["enemy_base"] = (t[0]-1, t[1]+1)
+                        return resultant_step
+        # else:
+        #         safe_positions.append("sw")
+
+        return (safe_positions,resultant_step)
                 
         
-        return randint(1,4)
+        #return randint(1,4)
 
-
-def ActBase(base):
-    '''
-    
-    Add your code here
-    Signal="XXYYSPTXTYSS enemy base at XXYY"; for blank keep space
-    XXYY=coord SP= special position SS special Structure EB-enemy Base
-    '''
-        def generate(s,t):
+def generate(s,t):
                 a,b = t
                 if s=="UP":
                         return(a,b+1)
@@ -57,29 +144,82 @@ def ActBase(base):
                 if s=="LT":
                         return(a-1,b)
                 if s=="NW":
-                        return(a-1,b+1) 
-        
-        if base.GetElixir() > 500:
-                base.create_robot('')
+                        return(a-1,b+1)
+
+
+
+def ActBase(base):
+        ebx="  "
+        eby="  "
+        basiN=" "*20
+        st="  "#enemy position
+        baso=base.GetYourSignal()
         sigli=base.GetListOfSignals()
-        ebx=00
-        eby=00
+        c,d=base.GetPosition()
+        if c<10:
+                e="0"+str(c)
+        if c>9:
+                e=str(c)
+        if d<10:
+                f="0"+str(d)
+        if d>9:
+                f=str(d)       
+                basiN+=e+f
+                basiN+=st
+                basiN+=ebx+eby
+        if baso[6:10]!=" "*4:
+                basiN=basiN[0:6]+baso[6:10]+basiN[10:]
+        
         for w in sigli:
                 if w[10:12]=="EB":
-                        =w[8:10]
-
-        st="  "
-        temp=""
-        temps=""
+                        ebx=w[6:8]
+                        eby=w[8:10]
+        if base.GetElixir() > 500:
+                base.create_robot('')
+        
         #Repeat 8 times in 8 dir
         if base.investigate_up()=="enemy":
                 st="UP"
-        
+                base.DeployVirus(800)
+        if base.investigate_nw()=="enemy":
+                st="NW"
+                base.DeployVirus(800)
+        if base.investigate_left()=="enemy":
+                st="LT"
+                base.DeployVirus(800)
+        if base.investigate_sw()=="enemy":
+                st="SW"
+                base.DeployVirus(800)
+        if base.investigate_down()=="enemy":
+                st="DW"
+                base.DeployVirus(800)
+        if base.investigate_se()=="enemy":
+                st="SE"
+                base.DeployVirus(800)
+        if base.investigate_right()=="enemy":
+                st="RT"
+                base.DeployVirus(800)
+        if base.investigate_ne()=="enemy":
+                st="NE"
+                base.DeployVirus(800)
+        c,d=base.GetPosition()
+        temps=""
+        e="  "
+        f="  "
         if st!="  ":
-                temp=base.GetPosition()
-                temps+=(string)temp[0]+(string)temp[1]
-                temps+=st
+                if c<10:
+                        e="0"+str(c)
+                if c>9:
+                        e=str(c)
+                if d<10:
+                        f="0"+str(d)
+                if d>9:
+                        f=str(d)       
+                basiN+=e+f
+                basiN+=st
+                basiN+=ebx+eby
                 
                 base.create_robot(temps)
         return
+
         
